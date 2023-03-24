cd4b5074c505a6d1ad8bd2db35b178aa5adb93fe
Alex Kiselich <alexander.kiselich@gmail.com>
2023-03-24 14:42:13 -0600

    Add 3 tests in node_spec.rb file

---
+++ spec/node_spec.rb
@@ -0,0 +1,20 @@
+require "rspec"
+require "./lib/node"
+
+RSpec.describe Node do
+  it "exisits" do
+    node = Node.new("plop")
+    expect(node).to be_an_instance_of(Node)
+  end
+
+  it "has data" do
+    node = Node.new("plop")
+
+    expect(node.data).to eq("plop")
+  end
+  it "has next node" do
+    node = Node.new("plop")
+    expect(node.next_node).to eq(nil)
+  end
+end
+    
