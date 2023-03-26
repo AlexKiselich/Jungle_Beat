b24de43036b4981f217a5cc8bd861b4072cea346
Alex Kiselich <alexander.kiselich@gmail.com>
2023-03-26 14:09:16 -0600

    Update link_list.rb and node.rb files

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,15 +1,38 @@
 class LinkedList
-  attr_reader :head
-  def initialize(head=nil)
-    @head = head
+  attr_reader :head, :count
+  def initialize
+    @head = nil
+    @count = 0
   end
 
   def append(data)
-    node = Node.new(data)
-    @head = node
-    "#{data}"
+    @count += 1
+    if @head == nil
+      @head = Node.new(data)
+    else
+      current_node = @head
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+      end
+      current_node.next_node = Node.new(data)
+      
+    end
+    data
   end
 
   def count
+    @count
+  end
+
+  def to_string
+    sounds = ""
+    current_node = @head
+    until current_node == nil
+      sounds = sounds + "#{current_node.data} "
+      current_node = current_node.next_node
+    end
+    sounds.rstrip
     
+    
+  end
 end
