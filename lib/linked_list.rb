d56a30152c9dcb43a89dda1c54988b3ee3c7023b
Alex Kiselich <alexander.kiselich@gmail.com>
2023-03-26 20:16:21 -0600

    Add an insert method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,5 @@
 class LinkedList
-  attr_reader :head, :count
+  attr_reader :head, :count, :next_node
   def initialize
     @head = nil
     @count = 0
@@ -32,7 +32,53 @@ class LinkedList
       current_node = current_node.next_node
     end
     sounds.rstrip
-    
-    
+    end
+
+
+  def prepend(data)
+    @count += 1
+    if @head == nil
+      @head = Node.new(data)
+    else
+      current_head = Node.new(data)
+      current_head.next_node = @head
+      @head = current_head
+    end
   end
-end
+
+  def insert(index, data)
+    @count +=1
+    current_node = @head
+    if current_node == nil
+      @head = Node.new(data)
+    else 
+      new_node = Node.new(data)
+      previous_node = current_node
+    index.times do
+      current_node = current_node.next_node
+      end
+    new_node.next_node = current_node
+    previous_node.next_node = new_node
+    end
+
+
+
+
+    def find(index, lenght)
+      found_sounds = ""
+      current_node = @head
+    index.times
+    end
+  end
+      
+
+
+
+
+
+
+ end
+
+
+
+
